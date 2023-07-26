823b3f3abd774bb1f2c2f2f8de8a7ab13a42318e
trizin <25263018+trizin@users.noreply.github.com>
2023-07-26 23:53:50 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+readme = ""
+
+setup(
+    author="oceanprotocol",
+    author_email="devops@oceanprotocol.com",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.8",
+    ],
+    description="Sapphire transaction wrapper",
+    install_requires=[],
+    name="pdr-sapphire-wrapper",
+    license="Apache Software License 2.0",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    url="https://github.com/oceanprotocol/pdr-sapphire-wrapper",
+    version='0.1.0',
+    zip_safe=False,
+    data_files=[('bin/sapphirewrapper.dylib', 'bin/sapphirewrapper.so')],  # Add your library files here
+)
