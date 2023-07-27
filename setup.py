86e8777a4b019d0a41d72df98ecd073ef8d2fa39
trizin <25263018+trizin@users.noreply.github.com>
2023-07-27 11:11:24 +0000

    Update version to 0.1.10

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         ]
     ),
     url="https://github.com/oceanprotocol/pdr-sapphire-wrapper",
-    version='0.1.9',
+    version='0.1.10',
     zip_safe=False,
     data_files=[('sapphire_wrapper_bin', ['bin/sapphirewrapper.dylib', 'bin/sapphirewrapper.so'])]
 )
