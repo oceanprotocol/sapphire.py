c51b2ec30b4f3d62149f9495a1c3670e3653ac67
alexcos20 <alex.coseru@gmail.com>
2023-07-30 09:45:36 +0300

    fix

--- go/main.go
+++ go/main.go
@@ -32,7 +32,7 @@ func SendETHTransaction(keyHexC *C.char, myAddrC *C.char, toAddrC *C.char, rpcUr
 	gasCostGwei := uint64(gasCostGweiC)
 	nonce := uint64(nonceC)
 	gasPrice := big.NewInt(0)
-	err := uint64(0)
+	
 	value := big.NewInt(int64(valueC))
 	value = value.Mul(value, big.NewInt(1000000000)) // convert gwei to wei
 
